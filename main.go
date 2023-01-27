5a09272ddc3aa840fca2c7331e41541c67b49824
git-shochann <sho_825.sa216@docomo.ne.jp>
2023-01-27 20:53:28 +0900

    add task1

---
+++ main.go
@@ -0,0 +1,58 @@
+// youtubeからタイトルに「SHOWROOM」が入ってるyoutubeの動画のURLを最新順に100件取得し出力
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"google.golang.org/api/option"
+	"google.golang.org/api/youtube/v3"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Printf("faild to load env : %v", err)
+	}
+	searchList()
+}
+
+func searchList() {
+
+	ctx := context.Background()
+	service, err := youtube.NewService(ctx, option.WithAPIKey(os.Getenv("YOUTUBE_API_KEY")))
+	if err != nil {
+		fmt.Printf("unable create service : %v", err)
+	}
+
+	var urls []string
+	var nextPageToken string
+
+	for i := 0; i <= 1; i++ {
+
+		call := service.Search.List([]string{"snippet"}).Q("SHOWROOM").Type("video").Order("date").MaxResults(50).PageToken(nextPageToken)
+
+		response, err := call.Do()
+		if err != nil {
+			log.Fatalf("Error making API call to list channels: %v", err.Error())
+		}
+
+		nextPageToken = response.NextPageToken
+
+		for _, searchResult := range response.Items {
+			url := fmt.Sprintf("https://www.youtube.com/watch?v=%v", searchResult.Id.VideoId)
+			urls = append(urls, url)
+		}
+
+	}
+
+	// 出力する
+	for _, v := range urls {
+		fmt.Println(v)
+	}
+
+}
